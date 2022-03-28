d49d0a626db4a7f455075952a2f3ea8b4ca2c678
Your Name <you@example.com>
2022-03-28 19:04:24 +0200

    Initial commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const hbs = require('express-handlebars');
+const {configuratorRouter} = require('./routes/configurator');
+const {homeRouter} = require('./routes/home');
+const {orderRouter} = require('./routes/order');
+const { handlebarsHelpers } = require('./utils/handlebars-helpers');
+
+const app = express();
+
+app.use(express.json());
+app.use(express.static('public'));
+app.use(cookieParser());
+app.engine('.hbs', hbs({
+  extname: '.hbs',
+  helpers: handlebarsHelpers,
+}));
+app.set('view engine', '.hbs');
+
+app.use('/', homeRouter);
+app.use('/configurator', configuratorRouter);
+app.use('/order', orderRouter);
+
+app.listen(3000, () => {
+  console.log('Server is listening on port 3000');
+});
